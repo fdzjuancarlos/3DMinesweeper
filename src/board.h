7f0abcf367e7d1944f439ac779aeb4190e30961d
fdzjuancarlos <fdz.juancarlos@gmail.com>
2014-10-27 13:37:06 +0100

    .h basico clase tablero

---
+++ src/board.h
@@ -0,0 +1,17 @@
+
+
+class Board
+{
+	public:
+		Board (int rows, int columns); //Difficult, doesn't create mines
+		virtual ~board ();
+		
+		int boxClicked(int x, int y);
+		generateMines();
+		std::string showContents();
+		
+
+	private:
+		Clase[][]
+
+};
